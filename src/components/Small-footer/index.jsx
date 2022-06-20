3f379ea3ec4d7ce29885f676f29b730245b17f26
Dark-Developer93 <eng.abdullahhasan2016@gmail.com>
2022-06-20 20:14:38 +0200

    Chore: upload the src folder

---
+++ src/components/Small-footer/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const SmallFooter = () => {
+  return (
+    <footer className="footer-half sub-bg">
+      <div className="container">
+        <div className="copyrights text-center mt-0">
+          <p>
+            © WeMaAd with ♥ in World. All Rights Reserved.
+            {/* <a href="#0">ThemesCamp</a>. */}
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default SmallFooter;
