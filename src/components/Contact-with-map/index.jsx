a785ad92b0102195fb7cb7de7ea67e7fd4e5ea70
Dark-Developer93 <eng.abdullahhasan2016@gmail.com>
2022-06-27 18:38:47 +0200

    Add: add the contact

--- src/components/Contact-with-map/index.jsx
+++ src/components/Contact-with-map/index.jsx
@@ -12,7 +12,38 @@ const ContactWithMap = () => {
     }
     return error;
   }
-  const sendMessage = (ms) => new Promise((r) => setTimeout(r, ms));
+  const validatePhone = (value) => {
+    let error;
+    if (!value) {
+      error = "Required";
+    } else if (!/^[0-9]{9,}$/i.test(value)) {
+      error = "Invalid phone number";
+    }
+    return error;
+  };
+
+  const sendMessage = ({ name, email, phone, message }) => {
+    let data = {
+      Name: name,
+      Email: email,
+      Phone: phone,
+      Message: message,
+      "Data Collection": "Accepted the data collection",
+      "Form Origin": "From WeMaAd Website the Contact Us Page",
+    };
+    // fetch the values of the email field and send it to the server
+    fetch("https://usebasin.com/f/f5aeb5b551e1", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  };
+
   return (
     <>
       <section className="contact section-padding">
@@ -26,18 +57,20 @@ const ContactWithMap = () => {
                   initialValues={{
                     name: "",
                     email: "",
+                    phone: "",
                     message: "",
                   }}
                   onSubmit={async (values) => {
-                    await sendMessage(500);
-                    alert(JSON.stringify(values, null, 2));
+                    await sendMessage(values);
+                    // alert(JSON.stringify(values, null, 2));
                     // show message
 
                     messageRef.current.innerText =
-                      "Your Message has been successfully sent. I will contact you soon.";
+                      "Your Message has been successfully sent. We will contact you soon.";
                     // Reset the values
                     values.name = "";
                     values.email = "";
+                    values.phone = "";
                     values.message = "";
                     // clear message
                     setTimeout(() => {
@@ -76,6 +109,20 @@ const ContactWithMap = () => {
 
                         <div className="form-group">
                           <Field
+                            validate={validatePhone}
+                            id="form_phone"
+                            type="tel"
+                            name="phone"
+                            placeholder="Phone with country code"
+                            required="required"
+                          />
+                          {errors.phone && touched.phone && (
+                            <div>{errors.phone}</div>
+                          )}
+                        </div>
+
+                        <div className="form-group">
+                          <Field
                             as="textarea"
                             id="form_message"
                             name="message"
@@ -83,6 +130,13 @@ const ContactWithMap = () => {
                             rows="4"
                             required="required"
                           />
+                        </div>
+
+                        <div className="form-group">
+                          <span>
+                            By submiting the form, I agree that my submitted
+                            data is being collected and stored.
+                          </span>
                         </div>
 
                         <button type="submit" className="btn-curve btn-lit">
@@ -102,11 +156,13 @@ const ContactWithMap = () => {
                 </h3>
                 <div className="item mb-40">
                   <h5>
-                    <a href="#0">Email@example.com</a>
+                    <a href="mailto:hello@wemaad.net">Hello@wemaad.net</a>
                   </h5>
-                  <h5>+4.930.705.5448</h5>
+                  <h5>
+                    <a href="tel:+201142549787">+20 114 254 9787</a>
+                  </h5>
                 </div>
-                <h3 className="custom-font wow" data-splitting>
+                {/* <h3 className="custom-font wow" data-splitting>
                   Visit Us.
                 </h3>
                 <div className="item">
@@ -115,19 +171,25 @@ const ContactWithMap = () => {
                     <br />
                     Melbourne, Australia
                   </h6>
-                </div>
+                </div> */}
                 <div className="social mt-50">
-                  <a href="#0" className="icon">
+                  <a href="https://www.facebook.com/" className="icon">
                     <i className="fab fa-facebook-f"></i>
                   </a>
-                  <a href="#0" className="icon">
+                  <a
+                    href="https://www.instagram.com/wemaad.llc/"
+                    className="icon"
+                  >
+                    <i className="fab fa-instagram"></i>
+                  </a>
+                  <a href="https://twitter.com/WemaadL" className="icon">
                     <i className="fab fa-twitter"></i>
                   </a>
-                  <a href="#0" className="icon">
-                    <i className="fab fa-pinterest"></i>
-                  </a>
-                  <a href="#0" className="icon">
-                    <i className="fab fa-behance"></i>
+                  <a
+                    href="https://www.linkedin.com/company/wemaad/"
+                    className="icon"
+                  >
+                    <i className="fab fa-linkedin"></i>
                   </a>
                 </div>
               </div>
@@ -137,7 +199,7 @@ const ContactWithMap = () => {
       </section>
       <div className="map" id="ieatmaps">
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d19868.687203718404!2d-0.14297520856388865!3d51.502466162777694!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47d8a00baf21de75%3A0x52963a5addd52a99!2sLondon%2C%20UK!5e0!3m2!1sen!2seg!4v1644772966009!5m2!1sen!2seg"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1726.633706773957!2d31.191688358100585!3d30.057868395469413!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x145841477414d38f%3A0x9968602e746c396!2sMit%20Akaba%2C%20Agouza%2C%20Giza%20Governorate%203752402!5e0!3m2!1sen!2seg!4v1656036647392!5m2!1sen!2seg"
           style={{ border: 0 }}
           allowFullScreen=""
           loading="lazy"
