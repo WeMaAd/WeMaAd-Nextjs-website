a67bd12ea17ae73f8cf1e6a17c7e3d159cb1309a
Dark-Developer93 <eng.abdullahhasan2016@gmail.com>
2022-06-28 18:07:09 +0200

    Edit: edit the works page

--- src/pages/works2/works2-dark.jsx
+++ src/pages/works2/works2-dark.jsx
@@ -53,7 +53,7 @@ const Works2Dark = () => {
             <div className="col-lg-9 col-md-11 static">
               <div className="capt mt-50">
                 <div className="parlx">
-                  <h2 className="custom-font">My amazing works</h2>
+                  <h2 className="custom-font">Our amazing works</h2>
                   <p>
                     Creative way to showcase your works at their absolute best.
                   </p>
