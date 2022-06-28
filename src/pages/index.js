bcdfee471901b185cf50a1efbcc818291eaf2df4
Dark-Developer93 <eng.abdullahhasan2016@gmail.com>
2022-06-28 18:04:01 +0200

    Edit: edit the first land home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,5 @@
-import DarkTheme from '../layouts/Dark'
-import Demos from "./demos";
+import HomeDark from "./home/home2-dark.jsx";
 
 export default function Home() {
-  return (
-    <DarkTheme>
-      <Demos />
-    </DarkTheme>
-  )
+  return <HomeDark />;
 }
