d2b8f6c63d2ed82d2deb5aff1bd062d7e2906551
Dark-Developer93 <eng.abdullahhasan2016@gmail.com>
2022-07-01 02:31:35 +0200

    Edit: edit the icon for mobile

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -24,6 +24,7 @@ class MyDocument extends Document {
           />
           <meta name="author" content="" />
           <link rel="shortcut icon" href="/img/atom-logo-light.svg" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/img/atom-logo-light.svg">
           {/* Google Fonts */}
           <link
             href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap"
